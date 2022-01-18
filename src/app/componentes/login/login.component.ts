bbf982a33842a906be50967217c9ff21693da2ab
Jaime Pardo <jdpm2014@gmail.com>
2022-01-18 01:02:53 -0300

    se integra el servicio de autenticacion tanto en base de datos cloudfire como en la aplicacion

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'flash-messages-angular';
+import { LoginService } from 'src/app/servicios/login.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  email:string
+  password:string
 
-  constructor() { }
+  constructor(private router:Router, private flashMessages:FlashMessagesService, private loginService:LoginService) { }
 
   ngOnInit(): void {
   }
-
+  login()
+  {
+    this.loginService.login(this.email, this.password).then(res =>{this.router.navigate(['/']);
+    }).catch(error => {this.flashMessages.show(error.message, 
+      {cssClass: 'alert-danger', timeout: 4000})})
+  }
 }
