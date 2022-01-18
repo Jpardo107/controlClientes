8043e1057a39cb2fd0ad96708a4efe16ffe61269
Jaime Pardo <jdpm2014@gmail.com>
2022-01-18 00:06:32 -0300

    se agrega funcionalidad de agregar, editar y eliminar registros de la base de datos

--- src/app/componentes/editar-cliente/editar-cliente.component.ts
+++ src/app/componentes/editar-cliente/editar-cliente.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FlashMessagesService } from 'flash-messages-angular';
+import { Cliente } from 'src/app/modelo/cliente.model';
+import { clienteServicio } from 'src/app/servicios/cliente.service';
 
 @Component({
   selector: 'app-editar-cliente',
@@ -6,10 +10,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editar-cliente.component.css']
 })
 export class EditarClienteComponent implements OnInit {
+  cliente:Cliente = 
+  {
+    nombre:'',
+    apellido:'',
+    email:'',
+    saldo:0
+  }
+  id:string
+  constructor(private clientesServicio:clienteServicio, private flashMessages: FlashMessagesService,
+              private router:Router, private route:ActivatedRoute) { }
 
-  constructor() { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params['id']
+    this.clientesServicio.getCliente(this.id).subscribe(cliente => 
+      {
+        this.cliente = cliente
+      })
+  }
+  
+  guardar({value, valid}:{value:Cliente, valid:Boolean})
+  {
+    if(!valid)
+    {
+      this.flashMessages.show('Por favor llena el formulario', {cssClass: 'alert-danger', timeout:4000})
+    }
+    else
+    {
+      value.id = this.id
+      this.clientesServicio.modificar(value)
+      this.router.navigate(['/'])
+    }
   }
 
+  eliminar()
+  {
+    if(confirm('¿Seguro desea eliminar este registro?'))
+    {
+      this.clientesServicio.eliminarCliente(this.cliente)
+      this.router.navigate(['/'])
+    }
+  }
 }
